3543ae8ef78321b6d637f26be45567d159bb6657
Dapeng Hu <damon.hu@gmail.com>
2019-03-06 17:22:49 +0800

    Before HTLC

--- virtualBank/contracts/VirtualBank.sol
+++ virtualBank/contracts/VirtualBank.sol
@@ -1,51 +1,73 @@
 pragma solidity ^0.5.1;
 
-import "./lib/external/openzeppelin-solidity/contracts/math/SafeMath.sol";
-import "./lib/external/openzeppelin-solidity/contracts/AddressUtils.sol";
-import "./lib/external/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "./lib/external/openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
+import "zeppelin-solidity/contracts/AddressUtils.sol";
+import "zeppelin-solidity/contracts/ECRecovery.sol";
 
 contract VirtualBank {
+    using SafeMath for uint256;
+    using ECRecovery for bytes32;
+    using AddressUtils for address;
+
     struct Client {
         address addr;
         uint balance; // 余额
         bool deposited; // 是否足额存款
     }
 
-    // 用户列表
-    Client[] customers;
-    
-    // 锁定期限
-    int lockPeriod;
+    struct Liquidation {
+        int32 lockPeriod;   // 锁定期限
+        int32 liquidateTime;  // 清盘时间
+        address waiverAddr;  // 豁免地址
+        int   master;        // 0 or 1. the index of the person who 
+        int   nounce;
+    }
 
-    // 状态: init, running, redemption, closed
-    int state;
+    // state of VirtualBank contract
+    enum State { Funding, Running, Liquidating, Closed }
 
-    // 清算人
-    int master;
+    // balance sheet
+    Client[] _clients;
 
-    // 清算时间
-    int clearingTime;
+    // liquidation information
+    Liquidation _liquidation;
 
-    // 清算人赎回公钥
-    bytes32 redepmtionPubKey;
+    State _state;
 
     // 构造函数，初始化参数Client[]， state
-    constructor(address a, address b, int valuea, int valueb){
+    constructor(address[] addrs, int[] balances, int32 lockPeriod){
+        require(addrs.length == 2);
+        require(addrs[0] != address(0) && addrs[1] != address(0));
+        require(balances.length == 2);
+        require(balances[0] > 0 && balances[1] > 0);
+        require(lock > 0);
+
+        _clients = new Client[2];
+        _clients[0] = Client(addrs[0], balances[0], false);
+        _clients[1] = Client(addrs[1], balances[1], false);
+
+        _liquidation = Liquidation(lock, 0, address(0), 0, 0);
+        _state = State.Funding;
     }
 
     function deposit() payable {
-        require(state == init);
+        require(state == State.Funding);
 
-        if(msg.sender == customers[0].addr && msg.value == customers[0].balance && !customers[0].deposited) {
-            customers[0].deposited = true;
-        } else if (msg.sender == customers[1].addr && msg.value == customers[1].balance && !customers[1].deposited) {
-            customers[1].deposited = true;
+        if(msg.sender == _clients[0].addr 
+         && msg.value == _clients[0].balance 
+         && !_clients[0].deposited) {
+            _clients[0].deposited = true;
+
+        } else if (msg.sender == _clients[1].addr 
+                 && msg.value == _clients[1].balance 
+                 && !_clients[1].deposited) {
+            _clients[1].deposited = true;
+
         } else {
             throw;
         }
 
-        if (customers[0].deposited && customers[1].deposited) {
+        if (_clients[0].deposited && _clients[1].deposited) {
             state = running;
         }
     }
@@ -53,47 +75,86 @@ contract VirtualBank {
     // 资产清算，
     // 输入参数: 最后的资产负债表，清算人的新地址, 双方的签名
     // 确定清算负责人，优先赎回权。
-    function claim() {
-        require(state == running)
+    function liquidate(int nounce, int32[] balances, address waiverAddr, bytes peerSignature) {
+        require(state == State.Running)
+        require(balances.length == 2);
+        require(waiverAddr != address(0));
 
-        // check both signatures
+        // identify master liquidator
+        int master, peer;
+        if (msg.sender == clients[0].addr) {
+            master = 0;
+            peer = 1;
+        } else (msg.sender == clients[1].addr) {
+            master = 1;
+            peer = 0;
+        } else {
+            throw;
+        }
 
-        // 确定清算人的index
-        master = ;
-        clearingTime = NOW;
-        state = redemption;
-        redepmtionPubKey = ;
+        // check peer's signature
+        bytes32 hash = keccak256(abi.encodePacked(address(this), nounce, balances[0], balances[1], waiverAddr));
+        require(checkSignature(hash, peerSignature, _clients[peer].addr));
 
-        // 资产分配
-        int value = customers[1 - master].balance;
-        customers[1 - master].balance = 0;
-        customers[1 - master].addr.send(value);
+        // check new balance sheet
+        require(balances[0] > 0 && balances[1] > 0);
+        require((balances[0] + balances[1]) == (_clients[0].balance + _clients[1].balance));
+
+        // 清盘
+        _liquidation.nounce = nounce;
+        _liquidation.master = master;
+        _liquidation.waiverAddr = waiverAddr;
+        _liquidation.liquidateTime = now;
+
+        state = State.Liquidating;
+
+        // 更新资产负债表，将 Peer 的资产返还给Peer，己方的资产留下
+        _clients[master].balance = balances[master];
+        _clients[peer].balance = balances[peer];
+        _clients[peer].addr.send(balances[peer]);
 
     }
 
-    // 由清算人发起请求，锁定期限之后可以赎回属于自己的资产
-    function withdrawByRedemption() {
-        require(state == redemption);
-        require(msg.sender == customers[master].addr);
-        require(NOW >= clearingTime);
+    // 由主清盘人发起请求，锁定期限之后可以赎回属于自己的资产
+    function withdrawByMaster() {
+        require(state == State.Liquidating);
+        int master = _liquidation.master;
+        require(msg.sender == _clients[master].addr);
+        require(NOW >= _liquidation.liquidateTime + _liquidation.lockPeriod);
 
         // update state;
-        state = closed;
-        int value = customers[master].balance;
-        customers[master].balance = 0;
+        state = State.Closed;
 
-        // transfer value 
-        customers[master].addr.send(value);
+        // send fund to master
+        int value = _clients[master].balance;
+        msg.sender.send(value);
     }
 
-    // 其它股东赎回，携带双方的赎回地址的签名，不需要时间限制
-    function withdrawByPeer() {
-        require(state == redemption);
-        require(msg.sender == customers[1 - master].addr);
-        // check signature of redepmtionPubKey
+    // 对方股东赎回，携带 Waiver 签名，不需要时间限制
+    function withdrawByPeer(int nounce, bytes waiverSignature) {
+        require(state == State.Liquidating);
+        int peer = 1 - _liquidation.master;
+        require(msg.sender == _clients[peer].addr);
 
-        // update status
-        state = closed;
-        int value = 
+        // check signature of redepmtionPubKey
+        bytes32 hash = keccak256(abi.encodePacked(address(this), nounce));
+        require(checkSignature(hash, waiverSignature, _liquidation.waiverAddr));
+
+        // update state;
+        state = State.Closed;
+
+        // send fund to peer
+        int value = _clients[master].balance;
+        _clients[peer].addr.send(value);
+    }
+
+    function checkSignature( bytes32 hash, bytes signature, address expectedAddr) internal pure returns (bool){
+
+        //bytes32 hash = keccak256(abi.encodePacked(owner, amount, nonce));
+        bytes32 messageHash = hash.toEthSignedMessageHash();
+
+        // Verify that the message's signer is the owner of the order
+        address signer = messageHash.recover(signature);
+        require(signer == expectedAddr);
     }
 }
